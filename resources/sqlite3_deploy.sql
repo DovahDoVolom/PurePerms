b26a3ebe69672188086f2dabc1a9c2ed1cd1655a
64FF00 <64ff00@gmail.com>
2015-10-17 22:46:45 +0900

    Moar ASCII arts :D

--- resources/sqlite3_deploy.sql
+++ resources/sqlite3_deploy.sql
@@ -1,3 +1,16 @@
+/*
+    PurePerms by 64FF00 (Twitter: @64FF00)
+
+      888  888    .d8888b.      d8888  8888888888 8888888888 .d8888b.   .d8888b.
+      888  888   d88P  Y88b    d8P888  888        888       d88P  Y88b d88P  Y88b
+    888888888888 888          d8P 888  888        888       888    888 888    888
+      888  888   888d888b.   d8P  888  8888888    8888888   888    888 888    888
+      888  888   888P "Y88b d88   888  888        888       888    888 888    888
+    888888888888 888    888 8888888888 888        888       888    888 888    888
+      888  888   Y88b  d88P       888  888        888       Y88b  d88P Y88b  d88P
+      888  888    "Y8888P"        888  888        888        "Y8888P"   "Y8888P"
+*/
+
 CREATE TABLE IF NOT EXISTS groups(
   groupName TEXT PRIMARY KEY NOT NULL,
   isDefault INTEGER NOT NULL DEFAULT 0,
